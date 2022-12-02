3b7a7d978f1a67b5132bc4be3e6bd5697ae99577
KevZ1209 <kevinbowditch11@gmail.com>
2022-12-01 18:28:21 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+$("h1").animate({
+    opacity: '1',
+    left: '0px'
+}, 3000)
+
+setTimeout(function() {
+    $(".joke1").animate({
+        top: '0px',
+        opacity: '0.7'
+    }, 3000)
+}, 3000)
+
+setTimeout(function() {
+    $(".joke2").animate({
+        top: '0px',
+        opacity: '0.7'
+    }, 3000)
+}, 6000)
+
+setTimeout(function() {
+    $(".joke3").animate({
+        top: '0px',
+        opacity: '0.7'
+    }, 3000)
+}, 9000)
+
+setTimeout(function() {
+    $(".joke4").animate({
+        top: '0px',
+        opacity: '0.7'
+    }, 3000)
+}, 12000)
+
+setTimeout(function() {
+    $(".joke5").animate({
+        top: '0px',
+        opacity: '0.7'
+    }, 3000)
+}, 15000)
+
+setTimeout(function() {
+    $(".joke6").animate({
+        top: '0px',
+        opacity: '0.7'
+    }, 3000)
+}, 18000)
+
+setTimeout(function() {
+    $(".joke7").animate({
+        top: '0px',
+        opacity: '0.7'
+    }, 3000)
+}, 21000)
